9dc59d3753ed1ab3a9d7aadc69aeccb2c43fb70d
Yusuf Çağlar <faf7faf7@gmail.com>
2020-05-02 14:08:29 +0300

    Initial Commit

---
+++ delegates/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  delegates
+//
+//  Created by Yusuf ÇAĞLAR on 02/05/2020.
+//  Copyright © 2020 Yusuf CAGLAR. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
